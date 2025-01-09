386f04082647befed28e81e5727983673bcd366e
Anurag Borisa <anuragborisa@gmail.com>
2025-01-09 22:34:43 +0530

    Initial Commit

---
+++ EdutainmentApp/EdutainmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentAppApp.swift
+//  EdutainmentApp
+//
+//  Created by Anurag on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EdutainmentAppTests/EdutainmentAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentAppTests.swift
+//  EdutainmentAppTests
+//
+//  Created by Anurag on 09/01/25.
+//
+
+import Testing
+@testable import EdutainmentApp
+
+struct EdutainmentAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
